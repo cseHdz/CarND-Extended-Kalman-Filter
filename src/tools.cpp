94501c9e5f211e47a2cde61d46db6fdf63c92969
csehdz <carlos.hdz@me.com>
2021-04-11 19:41:41 +0000

    Implemented tools.cpp

--- src/tools.cpp
+++ src/tools.cpp
@@ -47,8 +47,33 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-   * TODO:
-   * Calculate a Jacobian here.
-   */
+  
+  // Implementation of function as per CarND Lectures
+  
+  MatrixXd Hj(3,4);
+  
+  // Poisition and Velocity State
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
+
+  // Common terms across Jacobian matrix
+  float c1 = px*px+py*py; // (px^2 + py^2)
+  float c2 = sqrt(c1); // (px^2 + py^2)^1/2
+  float c3 = (c1*c2); // (px^2 + py^2)^3/2
+
+  // Ensure common term in denominator (px^2 + py^2) is not zero
+  // Otherwise - divison by zero error
+  if (fabs(c1) < 0.0001) {
+    cout << "CalculateJacobian () - Error - Division by Zero" << endl;
+    return Hj;
+  }
+
+  // Compute the Jacobian matrix
+  Hj << (px/c2), (py/c2), 0, 0,
+      -(py/c1), (px/c1), 0, 0,
+      py*(vx*py - vy*px)/c3, px*(px*vy - py*vx)/c3, px/c2, py/c2;
+
+  return Hj;
 }
